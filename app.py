93a53a3c59c7c63f0ecac3a537129ea22a54106b
Dillon Price <priced1112@gmail.com>
2025-04-29 13:56:34 -0600

    adjusted some things

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from typing import Annotated
 from langchain_ollama import ChatOllama
 from langchain_community.utilities import SQLDatabase
 from langchain_community.tools import QuerySQLDataBaseTool
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, FewShotPromptTemplate
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 
@@ -26,7 +26,7 @@ import zipfile
 
 load_dotenv()
 
-data_path = "data/"
+data_path = "data"
 zip_path = "generated.zip"
 
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
@@ -35,9 +35,13 @@ with zipfile.ZipFile(zip_path, 'r') as zip_ref:
 conn = sqlite3.connect('eew.db')
 
 csvs = glob.glob(f"{data_path}/generated/*.csv")
-
+# print(csvs)
 for csv in csvs:
-    df = pd.read_csv(csv)
+    if csv == 'data/generated/rcra_violations.csv':
+        df = pd.read_csv(csv)
+        df['REPORTING_YEAR'] = pd.to_datetime(df['DATE_VIOLATION_DETERMINED'], format='%Y-%m-%d').dt.year
+    else:
+        df = pd.read_csv(csv)
     if "Unnamed: 0" in df.columns:
         df.drop(columns=["Unnamed: 0"], inplace=True)
     table_name = csv.split("/")[-1].replace(".csv", "")
@@ -46,7 +50,7 @@ conn.close()
 
 llm = ChatOllama(
     model="llama3.1:8b",
-    temperature=0,
+    temperature=0.2,
     streaming=True
 )
 
@@ -60,9 +64,10 @@ You are a SQLite expert. Given an input question, follow these steps to create a
     - The table `county_political_reporting` contains information about the party in control of counties.
     - The table `rcra_violations` contains information about federally regulated facilities who have RCRA violations. Columns like `FAC_STATE`, `FAC_COUNTY`, and `FACILITY_NAME` are useful for filtering while `VIOLATION_TYPE_DESC` can be used to count the number of violations.
     - The table `toxic_emissions` contains information about toxic pollutant emissions from the facilities. Columns like `FAC_STATE`, `FAC_COUNTY`, `FACILITY_NAME` and `POLLUTANT_NAME` are useful for filtering while `ANNUAL_EMISSIONS` can be used to sum the total amount of emissions.
-    - DO NOT use the `toxic_emissions` table if the question is about RCRA violations and DO NOT use the `rcra_violations` table if the quesiton is about toxic pollutant emissions.
+    - DO NOT use the `toxic_emissions` table if the question is about RCRA violations and DO NOT use the `rcra_violations` table if the question is about toxic pollutant emissions.
 2. **Select the Appropriate Columns:**
    - Use only the columns relevant to the question.
+   - `year` or `REPORTING_YEAR` can be used for dates.
 3. **Understand any Filtering:**
    - Pay close attention to any filtering criteria in the question. For example, if the question specifies a particular year, state, county, facility name or pollutant ensure that the SQL query includes a `WHERE` clause to filter the data accordingly.
 4. **Construct the SQL Query:**
@@ -82,9 +87,46 @@ Return ONLY the SQLQuery with no other context given. Do not include the questio
     Answer: Final answer here
     
     Only use the following tables:
-    {table_info} 
+    {table_info}
+
+Below are a number of examples of questions and their corresponding SQL queries. 
+Use these examples to help you understand how to construct your own SQL queries. But do not copy them directly. 
+Instead, use them as a guide to create your own SQL queries based on the question you are given.
 '''
-sql_chain_prompt = PromptTemplate.from_template((sql_prompt_text + '\nQuestion: {input}'))
+examples = [
+        {
+                'input': "Which party was in control in Maricopa county in 2020?",
+                'query': "SELECT party_in_control FROM county_political_reporting WHERE county = 'MARICOPA' AND year = 2020;",
+        },
+        {
+                'input': "What are the total number of RCRA violations by state?",
+                'query': "SELECT FAC_STATE, count(*) as avg_violations FROM rcra_violations GROUP BY state;",
+        },
+        {
+                'input': "In 2018, how much much Ammonia was released in Illinois?",
+                'query': "SELECT sum(ANNUAL_EMMISIONS) FROM toxic_emissions WHERE POLLUTANT_NAME = 'Ammonia' AND FAC_STATE = 'IL' AND REPORTING_YEAR = 2018;",
+        },
+        {
+                'input': "Give me the names of the top 5 facilities in Texas by the number of RCRA violations",
+                'query': "SELECT FAC_NAME, COUNT(*) as total_violations FROM rcra_violations WHERE FAC_STATE = 'TX' GROUP BY FAC_NAME ORDER BY total_violations DESC LIMIT 5;",
+        }
+]
+
+# sql_chain_prompt = PromptTemplate.from_template((sql_prompt_text + '\nQuestion: {input}'))
+
+example_prompt = PromptTemplate.from_template("Example User input: {input}\nExample SQLQuery: {query}")
+# Below are a number of examples of questions and their corresponding SQL queries. 
+# Use these examples to help you understand how to construct your own SQL queries. But do not copy them directly. 
+# Instead, use them as a guide to create your own SQL queries based on the question you are given.
+sql_agent_prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        prefix=sql_prompt_text,
+        suffix="Question: {input}\nSQLQuery:",
+        input_variables=["input", "table_info", "dialect", "top_k"],
+)
+
+print(sql_agent_prompt)
 
 class State(TypedDict):
     question: str
@@ -98,7 +140,7 @@ class QueryOutput(TypedDict):
 
 def write_query(state: State):
     """Generate SQL query to fetch information."""
-    prompt = sql_chain_prompt.invoke(
+    prompt = sql_agent_prompt.invoke(
         {
             "dialect": db.dialect,
             "top_k": 10,
@@ -148,8 +190,13 @@ def invoke_sql_graph(st_messages, callables):
 st.title("Toxic Pollutants and RCRA Violations Assistant")
 st.image("images/banner_image.png", use_container_width=False)
 st.write(
-    "Hello! \n"
-    "I am a chatbot built off of publicly available data from the EPA that can help you understand more about pollutants and Resource Conservation and Recovery Act violations in your area."
+    """
+    Hello! \n
+    I am a bot that can help you with questions about Toxic Pollutant emissions and Resource Conservation and Recovery Act (RCRA) violations. \n
+    You can ask me about the following topics: \n
+    - Toxic Pollutant emissions by state, county, or facility name
+    - RCRA violations by state, county, or facility name
+    """
 )
 
 # if "expander_open" not in st.session_state:
